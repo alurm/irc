5a2cdcf1e609e970bb4658758a2e788a1612bfb5
Alan Urmancheev <alan.urman@gmail.com>
2024-01-21 19:55:06 +0400

    Format

--- test.cpp
+++ test.cpp
@@ -73,24 +73,24 @@ void pass_test() {
 				.in_trailing = false,
 			};
 
-			std::vector<lexeme> lexemes = lex_string("PASS\r\n", &state);
+		std::vector<lexeme> lexemes = lex_string("PASS\r\n", &state);
 
-			assert(lexemes.size() == 2);
-			assert(lexemes[0].tag == lexeme::word && strcmp(lexemes[0].value.word, "PASS") == 0);
-			assert(lexemes[1].tag == lexeme::carriage_return_line_feed);
+		assert(lexemes.size() == 2);
+		assert(lexemes[0].tag == lexeme::word && strcmp(lexemes[0].value.word, "PASS") == 0);
+		assert(lexemes[1].tag == lexeme::carriage_return_line_feed);
 
-			parse_state parse_state = {
-				.prefix = {
-					.has_value = false,
-				},
-			};
-			std::vector<parseme> parsemes = parse_lexeme_string(lexemes, &parse_state);
+		parse_state parse_state = {
+			.prefix = {
+				.has_value = false,
+			},
+		};
+		std::vector<parseme> parsemes = parse_lexeme_string(lexemes, &parse_state);
 
-			assert(parsemes.size() == 1 && parsemes[0].tag == parseme::message);
-			message m = parsemes[0].value.message;
-			assert(strcmp(m.command, "PASS") == 0);
+		assert(parsemes.size() == 1 && parsemes[0].tag == parseme::message);
+		message m = parsemes[0].value.message;
+		assert(strcmp(m.command, "PASS") == 0);
 
-			std::cout << "Test passed!\n";
+		std::cout << "Test passed!\n";
 	}
 }
 
