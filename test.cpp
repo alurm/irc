dcfd964f16fcabd576ec090f6c93391ff54c229a
Alan Urmancheev <alan.urman@gmail.com>
2024-01-21 20:21:47 +0400

    Move testpass.cpp to test.cpp

--- test.cpp
+++ test.cpp
@@ -76,8 +76,39 @@ void pass_test() {
 
 		assert(lexemes.size() == 2);
 		assert(lexemes[0].tag == lexeme::error && lexemes[0].value.error == lex_error::carriage_return_or_line_feed);
-		// assert(lexemes[1].tag == lexeme::carriage_return_line_feed);
+		// // assert(lexemes[1].tag == lexeme::carriage_return_line_feed);
 
+		// parse_state parse_state = {
+		// 	.prefix = {
+		// 		.has_value = false,
+		// 	},
+		// };
+		// std::vector<parseme> parsemes = parse_lexeme_string(lexemes, &parse_state);
+
+		// assert(parsemes.size() == 1 && parsemes[0].tag == parseme::message);
+		// message m = parsemes[0].value.message;
+		// assert(strcmp(m.command, "PASS") == 0);
+
+		std::cout << "Test passed!\n";
+	}
+
+	{
+		// Lex.
+
+		lex_state state = {
+			.state = lex_state::in_word,
+			.word = "",
+			.in_trailing = false,
+		};
+
+		// Test case: "PASS \r\n"
+		std::vector<lexeme> lexemes = lex_string("PASS \r\n", &state);
+
+		// Ensure correct lexing.
+		assert(lexemes.size() == 2);
+		assert(lexemes[0].tag == lexeme::word && strcmp(lexemes[0].value.word, "PASS") == 0);
+		assert(lexemes[1].tag == lexeme::carriage_return_line_feed);
+		// Parse.
 		parse_state parse_state = {
 			.prefix = {
 				.has_value = false,
@@ -85,10 +116,11 @@ void pass_test() {
 		};
 		std::vector<parseme> parsemes = parse_lexeme_string(lexemes, &parse_state);
 
+		// Ensure correct parsing.
 		assert(parsemes.size() == 1 && parsemes[0].tag == parseme::message);
 		message m = parsemes[0].value.message;
 		assert(strcmp(m.command, "PASS") == 0);
-
+		
 		std::cout << "Test passed!\n";
 	}
 }
