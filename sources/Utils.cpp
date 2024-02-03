330d189571a07d651c4d5abb93c9359aca4e1eb0
areviksol <arevmkrtich@gmail.com>
2024-02-03 15:12:41 +0400

    args check

--- sources/Utils.cpp
+++ sources/Utils.cpp
@@ -31,3 +31,38 @@ bool endsWithCRLF(const std::string& str) {
     }
     return false;
 }
+
+
+bool is_valid_args(std::string port, std::string password)
+{
+    int port_n = std::atoi(port.c_str());
+
+    if (password.empty() || password.length() < 8)
+    {
+        std::cout << "Error: password must contain at least 8 characters!" << std::endl;
+        return false;
+    }
+    
+    if (port.empty())
+    {
+        std::cout << "Error: empty port!" << std::endl;
+        return false;
+    }
+
+    for (size_t i = 0; i < port.size(); ++i)
+    {
+        if (!std::isdigit(port[i]))
+        {
+            std::cout << "Error: port cant contain non numeric characters!" << std::endl;
+            return false;
+        }
+    }
+
+    if (port_n <= 0 || port_n > 65535)
+    {
+        std::cout << "Error: port must be from  1 to 65535!" << std::endl;
+        return false;
+    }
+    
+    return true;
+}
--- sources/Utils.hpp
+++ sources/Utils.hpp
@@ -11,5 +11,5 @@ struct Server;
 int countParams(char **params);
 std::string trim(const std::string& str);
 bool endsWithCRLF(const std::string& str);
-
+bool is_valid_args(std::string port, std::string password);
 #endif
